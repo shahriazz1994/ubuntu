ea6a99ddc482c62075c2af0b5aa507c305c3fd58
polash <shahriazkobir@gmail.com>
2019-01-19 21:40:46 +0600

    Lot of work done. template and user side.

--- userauth/userauth/urls.py
+++ userauth/userauth/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from userauth import views
 
 urlpatterns = [
+    path('', views.landing_page, name='landing_page'),
     path('admin/', admin.site.urls),
+
+
 ]
